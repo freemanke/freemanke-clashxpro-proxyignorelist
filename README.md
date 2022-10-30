fc5b43085b63ee9a84ad460613c5edaee384059e
Freeman Ke <freemanke@qq.com>
2022-10-30 18:15:56 +0800

    dev

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# freemanke-clashxpro-proxyignorelist
+# 使用方法
+
+```
+ln -s ./proxyIgnoreList.plist /Users/freeman/.config/clash/proxyIgnoreList.plist
+```
---
+++ set.sh
@@ -0,0 +1,4 @@
+rm /Users/freeman/.config/clash/proxyIgnoreList.plist
+ln  
+/Users/freeman/Projects/freemanke-clashxpro-proxyignorelist/proxyIgnoreList.plist 
+/Users/freeman/.config/clash/proxyIgnoreList.plist
